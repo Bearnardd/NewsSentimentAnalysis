11586f15607c435b6f9601b6814755aae5d3ce6f
bartekkz <bartek.szmelczynski@gmail.com>
2019-06-10 23:10:33 +0200

    update cleaner class

--- cleaner.py
+++ cleaner.py
@@ -11,110 +11,24 @@ from nltk import word_tokenize
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
+
 class Cleaner:
-  def __init__(self, df):
-    self.df = df
-  
-  def create_dataset(self, column):
-    self.text = list(self.df[column])
-    new_words = []
-		
+  def __init__(self, text):
+    self.lemmatizer = WordNetLemmatizer()
+    self.stop_words = set(stopwords.words('english'))
+    
+  def clean_data(self, text):
+    text = re.sub('[^\w\s]', '', text, re.UNICODE)
+    text = text.lower()
+    text = [self.lemmatizer.lemmatize(token) for token in text.split(' ')]
+    text = [self.lemmatizer.lemmatize(token, 'v') for token in text]
+    text = [word for word in text if not word in self.stop_words]
+    text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    text = ' '.join(text)
+    return text
 
+     
 
-  def remove_non_ascii(self):
-    new_words = []
-    for word in self.text:
-      new_word = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore')
-      new_words.append(new_word)
-    self.text = new_words
-
-		
-  def to_lowercase(self):
-    new_words = []
-    for word in self.text:
-      new_word = word.lower()
-      new_words.append(new_word)
- 
-    self.text = new_words
-
-
-  def remove_punctuation(self):
-    new_words = []
-    for word in self.text:
-      new_word = re.sub(r'[^\w\s]', '', word)
-      if new_word != '':
-        new_words.append(new_word)
-    self.text = new_words
-
-
-  def replace_numbers(self):
-    p = inflect.engine()
-    new_words = []
-    for word in self.text:
-      if word.isdigit():
-        new_word = p.number_to_words(word)
-        new_words.append(new_word)
-      else:
-        new_words.append(word)
-    self.text = new_words
-
-  
-  def remove_stopwords(self):
-    new_words = []
-    for word in self.text:
-      if word not in stopwords.words('english'):
-        new_words.append(word)
-    self.text = new_words
-
-
-  def lemmatize_verbs(self):
-    lemmatizer = WordNetLemmatizer()
-    lemmas = []
-    for word in self.text:
-      lemma = lemmatizer.lemmatize(word, pos='v')
-      lemmas.append(lemma)
-    self.text = lemmas
-
-
-  def normalize(self):
-    new_words = []
-    self.remove_non_ascii()
-    self.to_lowercase()
-    self.remove_punctuation()
-    self.replace_numbers()
-    self.remove_stopwords()
-    self.lemmatize_verbs()
-    for text in self.text:
-      new_text = word_tokenize(text)
-      new_words.append(new_text)
-    self.text = new_words
-  
-
-  def word2idx(self):
-    '''
-    Takes as input tokenized text and
-    return word2idx dict
-    '''
-    text_all = [' '.join(line) for line in self.text]
-    all_reviews = ' '.join(text_all)
-    words = all_reviews.split()
-    total_words = len(words)
-    count_words = Counter(words)
-    sorted_words = count_words.most_common(total_words)
-    self.word2idx = {w:i+1 for i, (w,c) in enumerate(sorted_words)}
-
-
-  def encode_reviews(self):
-    self.reviews_int = []
-    for review in self.text:
-      r = [self.word2idx[word] for word in review]
-      self.reviews_int.append(r)
-
-  
-  def to_pad(self, tokenizer, maxlen):
-    seq = tokenizer.texts_to_sequences(self.text)
-    pad = pad_sequences(seq, maxlen=maxlen)
-    return pad
    
     
 
