dcf05f9de576b8e9fae824713e02e65a8a66d62d
bartekkz <bartek.szmelczynski@gmail.com>
2019-06-10 23:11:43 +0200

    update main file referring to cleaner class

--- main.py
+++ main.py
@@ -10,14 +10,23 @@
 
 
 from newsparser import NewsParser
+from network import Network
 from cleaner import Cleaner
+from keras.preprocessing.text import Tokenizer
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.sequence import pad_sequences
 import pandas as pd
 import numpy as np
 import os
+import warnings
+warnings.filterwarnings('ignore')
     
 data_dir = os.path.join(os.getcwd(), 'data/imdb_master.csv')
 dow_path = '/home/bartek/Downloads/'
 glove_dir = os.path.join(dow_path, 'glove.6B.50d.txt')
+maxlen = 130
+max_words = 10000
+emb_size = 128 
 
 def get_data(path):
   data = pd.read_csv(path, encoding='latin-1')
@@ -62,11 +71,54 @@ def create_emb_matrix(emb_idx, tokenizer, emb_size):
 
 
 
+
+
 path = '/home/bartek/Desktop/Projects/Python/NewsSentimentAnalysis/data/news.db'
 topics = ['Love', 'Hate', 'Kill', 'Happy']
 labels = [True, False, False, True]
 if __name__ == '__main__':
-  loadGloveModel(glove_dir)
+  cleaner = Cleaner()
+  df = get_data(data_dir)  
+  df['processed_reviews'] = df.review.apply(lambda x: cleaner.clean_data(x))
+
+  tokenizer = Tokenizer(max_words)
+  tokenizer.fit_on_texts(df['processed_reviews'])
+  list_tokenizer_train = tokenizer.texts_to_sequences(df['processed_reviews'])
+
+  X_t = pad_sequences(list_tokenizer_train, maxlen)
+  y = df['label']
+
+  embedding_index = loadGloveModel(glove_dir)
+  embedding_matrix = create_emb_matrix(embedding_index, tokenizer, 128)
+
+  network = Network(max_words, maxlen, emb_size, embedding_matrix)
+  X_train, X_test, y_train, y_test = train_test_split(X_t, y, test_size=0.2, random_state=123)
+
+
+  network._model.fit(X_train,
+                   y_train,
+                   epochs=3,
+                   validation_data=(X_test, y_test))
+
+
+  
+
+  
+
+
+
+
+  '''
+
+  '''
+
+
+      
+  
+
+
+
+
 
   
 
@@ -99,6 +151,19 @@ if __name__ == '__main__':
     parser.get_data(topic, label, 5, True)
   parser.disconnect()
   print('DONE!')
+  network._model.fit(X_train,
+                     y_train,
+                     epochs=3,
+                     batch_size=16,
+                     validation_data=(X_test, y_test))
+
+
+  tokenizer = Tokenizer(maxlen)
+  tokenizer.fit_on_texts(cleaner.text)
+  X = cleaner.to_pad(tokenizer, maxlen)
+  y = data['label'].values
+  print(embedding_matrix[0])
+  print()
   '''
 
 
