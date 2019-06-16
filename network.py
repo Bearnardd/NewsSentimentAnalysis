943d47bbb30e0013170daf76ac0ef1e8ddbb9401
bartekkz <bartek.szmelczynski@gmail.com>
2019-06-16 21:27:42 +0200

    add predict_news function

--- network.py
+++ network.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python3
 
-import tensorflow as tf
-from tensorflow.keras.layers import (Dense, LSTM, Embedding, GlobalMaxPool1D,
+
+from keras.layers import (Dense, LSTM, Embedding, GlobalMaxPool1D,
                          Dropout, Input, Bidirectional, BatchNormalization)
-from tensorflow.keras.backend import binary_crossentropy
-from tensorflow.keras.models import Model
+from keras.models import Model, load_model
+from keras.preprocessing.text import Tokenizer
 import warnings
+from cleaner import Cleaner
+from newsparser import NewsParser
+import sqlite3
+
 warnings.filterwarnings('ignore')
 
 
 class Network:
-  def __init__(self, max_words, maxlen, embeded_size, embedding_matrix, 
+  def __init__(self, max_words, maxlen, embeded_size, embedding_matrix=None, 
           new=True, path=None):
     self.max_words = max_words
     self.maxlen = maxlen
+    self.parser = NewsParser()
+    self.cleaner = Cleaner()
+    self.tokenizer = Tokenizer(self.max_words)
     self.embeded_size = embeded_size
     self.embedding_matrix = embedding_matrix
     if new:
         self._model = self.create_model()
     else:
         self._model = load_model(path)
+
+
+  def connect_to_db(self, path):
+    self.conn = sqlite3.connect(path)
+    self.c = self.conn.cursor()
 
 
   def create_model(self):
@@ -41,14 +53,30 @@ class Network:
     return model
 
 
-
   def fit_data(self, X, y, epochs, batch_size, validation_split, save=False, path=None):
-    
     self._model.fit(X, y, epochs=epochs, batch_size=batch_size,
                     validation_split=validation_split)
     if save:
       if path==None:
         path = input('Path for weights:\n')
       self._model.save(path)
+  
 
+  def predict_news(self, topic, page_size, database=True):
+    self.parser.get_data(topic=topic, page_size=page_size)
+    for page in range(page_size):
+        data = self.cleaner.clean_data(self.parser.titles[page])
+        pad = self.cleaner.to_pad(data, self.tokenizer, self.maxlen)
+        ans = self._model.predict(pad)
+        if ans <= 0.5:
+          label = 'POSITIVE'
+        else:
+          label = 'NEGATIVE'
+        
+        if database:
+          self.c.execute('INSERT INTO news VALUES(?, ?, ?, ?)', (self.parser.titles[page],          self.parser.desc[page], self.parser.content[page], label))
+          self.conn.commit()  
+
+
+    
 
